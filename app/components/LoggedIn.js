06f1eb0b852191b8d7a00528fe464ac8e7a70a42
Andrew <ceo@allow2.com>
2019-03-12 07:23:55 +1000

    add 'no devices' screen and cleanup for 1.2.0

--- app/components/LoggedIn.js
+++ app/components/LoggedIn.js
@@ -208,62 +208,73 @@ export default class LoggedIn extends Component {
                     />
                 <Tabs>
                     <Tab label="Devices" >
+                        { devices.supported.length < 1 &&
+                            <div style={{ textAlign: "center" }}>
+                                <h1>No Devices Found</h1>
+                                <p style={{ width:"75%", margin: "auto" }}>Allow2Automate will auto-discover Wemo devices on your network and list them here when found.</p>
+                            </div>
+                        }
+                        { devices.supported.length > 0 &&
                         <Table>
                             <TableBody
                                 displayRowCheckbox={false}
                                 showRowHover={true}
                                 stripedRows={true}>
-                            { devices.supported.map( function(device) {
-                                    let token = deviceTokens[device.device.device.modelName];
-                                    let imageName = deviceImages[device.device.device.modelName];
-                                    let paired = this.props.pairings[device.device.UDN];
-                                    let child = paired && paired.ChildId && this.props.children[paired.ChildId];
-                                    let detail = child ? (
+                                { devices.supported.map(function (device) {
+                                        let token = deviceTokens[device.device.device.modelName];
+                                        let imageName = deviceImages[device.device.device.modelName];
+                                        let paired = this.props.pairings[device.device.UDN];
+                                        let child = paired && paired.ChildId && this.props.children[paired.ChildId];
+                                        let detail = child ? (
                                             <b>{child.name}</b>
                                         ) : <b>Paired</b>;
-                                    let url = child && allow2AvatarURL(null, child);
-                                    return (
-                                        <TableRow
-                                            key={device.device.UDN}
-                                            selectable={false}>
-                                            <TableRowColumn>
-                                                { imageName &&
-                                                <img width="40" height="40" src={ 'assets/img/' + imageName + '.png' } />
-                                                }
-                                            </TableRowColumn>
-                                            <TableRowColumn>
-                                                { token &&
-                                                <span>{ device.device.device.friendlyName }</span>
-                                                }
-                                                { !token &&
-                                                <span><i style={{ color: '#555555' }}>{ device.device.device.friendlyName }</i></span>
-                                                }
-                                            </TableRowColumn>
-                                            <TableRowColumn style={{textAlign: 'center'}}>
-                                                <Checkbox
-                                                    label=''
-                                                    isChecked={device.state}
-                                                    isDisabled={!token || device.active ? true : false}
-                                                    handleCheckboxChange={this.toggleCheckbox.bind(this, device)}
-                                                    />
-                                            </TableRowColumn>
-                                            <TableRowColumn style={{textAlign: 'right'}}>
-                                                { child &&
-                                                <Avatar src={url} />
-                                                }
-                                            </TableRowColumn>
-                                            <TableRowColumn style={{textAlign: 'left'}}>
-                                                { paired && detail }
-                                                { !paired &&
-                                                <FlatButton label="Assign" onClick={this.assign.bind(this, device.device, token)} />
-                                                }
-                                            </TableRowColumn>
-                                        </TableRow>
-                                    );
-                                }.bind(this)
-                            )}
+                                        let url = child && allow2AvatarURL(null, child);
+                                        return (
+                                            <TableRow
+                                                key={device.device.UDN}
+                                                selectable={false}>
+                                                <TableRowColumn>
+                                                    { imageName &&
+                                                    <img width="40" height="40"
+                                                         src={ 'assets/img/' + imageName + '.png' }/>
+                                                    }
+                                                </TableRowColumn>
+                                                <TableRowColumn>
+                                                    { token &&
+                                                    <span>{ device.device.device.friendlyName }</span>
+                                                    }
+                                                    { !token &&
+                                                    <span><i
+                                                        style={{ color: '#555555' }}>{ device.device.device.friendlyName }</i></span>
+                                                    }
+                                                </TableRowColumn>
+                                                <TableRowColumn style={{textAlign: 'center'}}>
+                                                    <Checkbox
+                                                        label=''
+                                                        isChecked={device.state}
+                                                        isDisabled={!token || device.active ? true : false}
+                                                        handleCheckboxChange={this.toggleCheckbox.bind(this, device)}
+                                                        />
+                                                </TableRowColumn>
+                                                <TableRowColumn style={{textAlign: 'right'}}>
+                                                    { child &&
+                                                    <Avatar src={url}/>
+                                                    }
+                                                </TableRowColumn>
+                                                <TableRowColumn style={{textAlign: 'left'}}>
+                                                    { paired && detail }
+                                                    { !paired &&
+                                                    <FlatButton label="Assign"
+                                                                onClick={this.assign.bind(this, device.device, token)}/>
+                                                    }
+                                                </TableRowColumn>
+                                            </TableRow>
+                                        );
+                                    }.bind(this)
+                                )}
                             </TableBody>
                         </Table>
+                        }
                     </Tab>
                     { devices.notSupported.length > 0 &&
                     <Tab label="Unsupported" >
